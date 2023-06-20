3fb0356ec2893c5948580936c1a666304b0aa98a
Estime10 <estimevangu.pro@gmail.com>
2023-06-20 12:47:00 +0200

    shipping added and tested

--- server/server.js
+++ server/server.js
@@ -15,10 +15,55 @@ const stripe = require('stripe')
 app.post('/checkout', async (req, res, next) => {
   try {
       const session =  await stripe.checkout.sessions.create({
-          payment_method_types: ['card'],
+        payment_method_types: ['card'],
+        shipping_address_collection: {
+          allowed_countries: ['BE', 'FR', 'LU', 'NL', 'DE'],
+        },
+        shipping_options: [
+          {
+            shipping_rate_data: {
+              type: 'fixed_amount',
+              fixed_amount: {
+                amount: 0,
+                currency: 'eur',
+              },
+              display_name: 'Free shipping',
+              delivery_estimate: {
+                minimum: {
+                  unit: 'business_day',
+                  value: 5,
+                },
+                maximum: {
+                  unit: 'business_day',
+                  value: 7,
+                },
+              },
+            },
+          },
+          {
+            shipping_rate_data: {
+              type: 'fixed_amount',
+              fixed_amount: {
+                amount: 1500,
+                currency: 'eur',
+              },
+              display_name: 'Next day air',
+              delivery_estimate: {
+                minimum: {
+                  unit: 'business_day',
+                  value: 1,
+                },
+                maximum: {
+                  unit: 'business_day',
+                  value: 1,
+                },
+              },
+            },
+          },
+        ],
           line_items: req.body.items.map((item) => ({
               price_data: {
-                  currency: 'eur',
+                  currency:'eur',
                   product_data: {
                       name: item.name,
                       images: [item.product],
